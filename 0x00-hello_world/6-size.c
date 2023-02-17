f8523a9b7653e74b7cdb4a4d25add06a4f3d2ad3
Qhojoblinks-7 <immanueleshun9@gmail.com>
2023-02-17 09:25:40 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,12 +10,11 @@ int main(void)
 {
 
 	/*sizeof oprator is used to evaluate the size osf a variable*/
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
 	printf("Size of int: %lu byte(s)\n", sizeof(int));
-	printf("Size of long: %lu byte(s)\n", sizeof(long));
-	printf("size of float: %lu byte(s)\n", sizeof(float));
-	printf("size of char: %lu byte(s)\n", sizeof(char));
-	printf("size of double: %lu byte(s)\n", sizeof(double));
+	printf("size of long: %lu byte(s)\n", sizeof(long));
 	printf("size of long long: %lu byte(s)\n", sizeof(long long));
+	printf("size of float: %lu byte(s)\n", sizeof(float));
 
 	return (0);
 }
