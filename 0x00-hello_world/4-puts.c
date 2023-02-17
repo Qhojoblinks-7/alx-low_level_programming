9aa32c72d7f8568dd5ec44dba7a477362e217e3d
Qhojoblinks-7 <immanueleshun9@gmail.com>
2023-02-17 05:10:17 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this isthe function of the main
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
