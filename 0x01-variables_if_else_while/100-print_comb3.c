6fd1d9b50c66764b74cb48db75b623d80841718c
Qhojoblinlks-7 <immanueleshun9@gmail.com>
2023-02-20 05:52:43 -0800

    combo3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - print all combo of two numbers posible
+ * followed by ',' and ' '
+ * and since they are they are to contain diff numbers
+ * use the variable m and n
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n, m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		for (m = 48; m <= 57; m++)
+		{
+			putchar(n);
+			putchar(m);
+			if (n != 57 || m != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
