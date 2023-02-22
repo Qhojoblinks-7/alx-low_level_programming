1b49768bf7f81b7154eb409168b652e011c1472f
Qhojoblinks-7 <immanueleshun9@gmail.com>
2023-02-21 21:47:51 -0800

    print alpshabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_alphabet -  print alphabets in lowecases followed by a new line
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+		{
+			_putchar('\n');
+		}
+	}
+}
+
