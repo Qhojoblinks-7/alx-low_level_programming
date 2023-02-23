da0d9f28317e388f1a62502312f44fe1f1a96089
Qhojoblinks-7 <immanueleshun9@gmail.com>
2023-02-22 21:57:32 -0800

    jack

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,19 +12,19 @@ void times_table(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			k = j*i;
+			k = j * i;
 			if (j == 0)
 			{
 				_putchar(k + '0');
 			}
-			if (k < 10 && j !=0)
+			if (k < 10 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
 				_putchar((k % 10) + '0');
 			}
-			else if (k >= 10)
+				else if (k >= 10)
 				{
 					_putchar(',');
 					_putchar(' ');
@@ -35,4 +35,3 @@ void times_table(void)
 		_putchar('\n');
 	}
 }
-
