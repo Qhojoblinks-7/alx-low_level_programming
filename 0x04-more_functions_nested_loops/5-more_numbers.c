afd51afc8625d0155124031a5517a1bac4861e46
Qhojoblinks-7 <immanueleshun9@gmail.com>
2023-02-23 18:07:28 -0800

    numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * more_numbers - prints 10 times the numbers, from 0 to 14, followed by a new line.
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * followed by a new line.
  */
 
 void more_numbers(void)
@@ -10,13 +11,12 @@ void more_numbers(void)
 
 	for (i = 0; i <= 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
-		{
-			if (j >= 10)
-				_putchar(j / 10 + '0');
-			_putchar(j % 10 + '0');
-		}
-		 _putchar('\n');
+	for (j = 0; j <= 14; j++)
+	{
+		if (j >= 10)
+			_putchar(j / 10 + '0');
+		_putchar(j % 10 + '0');
+	}
+	 _putchar('\n');
 	}
 }
-
