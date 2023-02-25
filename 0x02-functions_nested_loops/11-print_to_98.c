6e3777a4059038ad1005c95bff8d472b28c28099
Qhojoblinks-7 <immanueleshun9@gmail.com>
2023-02-25 00:54:45 -0800

    natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -22,6 +22,7 @@ void print_to_98(int n)
 			printf("%d", i);
 			if (i != 98)
 				printf(",");
+				printf(" ");
 		}
 		printf("\n");
 	}
@@ -32,6 +33,7 @@ void print_to_98(int n)
 			printf("%d", i);
 				if (i != 98)
 					printf(",");
+				printf(" ");
 		}
 		printf("\n");
 	}
