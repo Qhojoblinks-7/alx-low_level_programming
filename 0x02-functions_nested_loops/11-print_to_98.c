223b684f44f218d114d11fc6c020bcdd3e704b3d
Qhojoblinks-7 <immanueleshun9@gmail.com>
2023-02-25 01:33:26 -0800

    natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,32 +2,37 @@
 #include "main.h"
 
 /**
- * print_to_98 - prints natural numbers from n to 98
- * numbers shuld be folowed by a new line and a comma
- * numbers should be in order
+ * print_to_98 - a function that prints natural numbers from n to 98
+ * numbers should be followed by a comma and a space
+ * numbers should be in ascending order
  * the first printed number should be passed to the function
  * the last printed number should be 98
- * @n: starting number to print from
- * Return: 0 always
+ * @n: function parameter
+ * Return: void
  */
 
 void print_to_98(int n)
 {
 	int i;
 
-	if (n <= 98)
+	if (n >= 0 && n <= 98)
 	{
 		for (i = n ; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
 		}
+		printf("\n");
 	}
-	else
+	else if (n > 98)
 	{
-		for (i = n; i >= 98; i--)
+		for (i = n ; i >= 98; i--)
 		{
-			printf("%d, ", i);
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
 		}
-		printf("98\n");
+		printf("\n");
 	}
 }
