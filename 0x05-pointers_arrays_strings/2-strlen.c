508e93fe11d363d6c051e8f7366052165c479c17
Qhojojblinks-7 <immanueleshun9@gmail.com>
2023-02-27 15:59:21 -0800

    a function that returns the lenght of a sring

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that retuens the lemght of a string
+ * init lenght;
+ * while s is an array of a of the string (i.e s{})
+ * i is increamented
+ * return the lenght
+ * @s: string to evaluate
+ * Return: the lenght of the string
+ */
+
+int _strlen(char *s)
+{
+	int lenght;
+
+	while (s[lenght] != '\0')
+	{
+	lenght++;
+	}
+	return (lenght);
+}
+
