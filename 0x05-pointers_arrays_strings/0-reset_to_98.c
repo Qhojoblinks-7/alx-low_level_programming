a5af13f74b1057df0543e0ae91629b86c4b5490c
Qhojojblinks-7 <immanueleshun9@gmail.com>
2023-02-27 14:45:09 -0800

    updating a character

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a afunction that print  a parameter
+ * and updates the value with it points to 98
+ * @n: the character to be updated
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
