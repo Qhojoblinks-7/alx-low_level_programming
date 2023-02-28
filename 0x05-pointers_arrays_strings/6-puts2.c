34e5fd69ccec1b17ca3fdd8dccf28066a93900b9
Qhojojblinks-7 <immanueleshun9@gmail.com>
2023-02-28 03:24:25 -0800

    printing the characters of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - a function that prints every character of a string,
+ * starting from the first character
+ * followed by a new line
+ * @str: the string to be printed
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
