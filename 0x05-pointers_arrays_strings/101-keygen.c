df228ef6bb5f649ff02828e375d5f1b379ca0e06
Qhojojblinks-7 <immanueleshun9@gmail.com>
2023-02-28 19:42:58 -0800

    a program that generates a random password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -12,33 +12,25 @@
 
 int main (void)
 {
-	char password[PASSWORD_LENGTH + 1];
-	int i, random_number;
+	int pass[100];
+	int i, sum, n;
 
+	sum = 0;
 
 	srand(time(NULL));
 
-	for (i = 0; i < PASSWORD_LENGTH; i++)
+	for (i = 0; i < 100; i++)
 	{
-		random_number = rand() % 62;
-
-		if (random_number < 26)
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
 		{
-			password[i] = 'a' + random_number;
-		}
-		else if (random_number < 52)
-		{
-			password[i] = 'A' + random_number - 26;
-		}
-		else
-		{
-			password[i] = '0' + random_number - 52;
+			n = 2772 - sum - '0';
+				sum += n;
+			putchar(n + '0');
+			break;
 		}
 	}
-
-	password [PASSWORD_LENGTH] = '\0';
-
-	printf("%s\n", password);
-
 	return (0);
 }
