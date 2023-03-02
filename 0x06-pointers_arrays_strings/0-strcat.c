1c1624148d7b2afc3947cd5137ec88996f2d297f
Qhojojblinks-7 <immanueleshun9@gmail.com>
2023-03-02 13:52:40 -0800

    cat using strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,19 +14,19 @@ char *_strcat(char *dest, char *src)
 
 	a = b = 0;
 
-	for (a = 0; dest[a] != '\0'; a++)
-	{
+	while (dest[a] != '\0')
+	a++;
 
 
-	for (b = 0; src[b] != '\0'; b++)
-
+	while (src[b] != '\0')
 	{
 		dest[a] = src[b];
+		a++;
+		b++;
 	}
 
 	dest[a] = '\0';
-	}
 
 	return (dest);
-	
+
 }
