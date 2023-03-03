90986f67838a4e184fe5ebdb13f4bb38a65e1adf
Qhojojblinks-7 <immanueleshun9@gmail.com>
2023-03-03 06:42:50 -0800

    turning letters into figures

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *leet - a function that encodes a string into 1337
+ * @str: the string to be encoded
+ *
+ * Return: the resulting string
+ */
+
+char *leet(char *str)
+{
+	char *leet_char = "AaEeOoTtLl";
+	char *leet_code = "4433007711";
+	char *c = str;
+	int i, j;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; c[j] != '\0'; j++)
+		{
+			if (c[i] == leet_char[j] || c[i] == leet_char[j] + 32)
+			{
+				c[i] = leet_code[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
