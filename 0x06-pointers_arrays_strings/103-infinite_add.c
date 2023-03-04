8df1fbd0192d5f5b3b7295618cc29c9af6d1ce39
Qhojojblinks-7 <immanueleshun9@gmail.com>
2023-03-03 20:56:45 -0800

    infint additiom

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,37 +9,45 @@
  *
  * Return: pointer to the result or 0 if the result cannot be stored in r
  */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, k, carry, sum, len1, len2;
-	
-	for (len1 = 0; n1[len1] != '\0'; len1++)
-		;
-	for (len2 = 0; n2[len2] != '\0'; len2++)
-		;
+	int len1 = strlen(n1);
+	int len2 = strlen(n2);
 
-	if (len1 + len2 >= size_r)
-		return (0);
 
-	carry = 0;
+	if (len1 + len2 > size_r)
+	return (0);
 
-	for (i = len1 - 1, j = len2 - 1, k = 0; i >= 0 || j >= 0 || carry; i--, j--, k++)
+	int carry = 0;
+	int i = len1 - 1;
+	int j = len2 - 1;
+	int k = 0;
+
+	while (i >= 0 || j >= 0 || carry > 0)
 	{
-		sum = carry;
-		if (i >= 0)
-			sum += n1[i] - '0';
-		if (j >= 0)
-			sum += n2[j] - '0';
-		carry = sum / 10;
-		r[k] = sum % 10 + '0';
+
+	int digit1 = i >= 0 ? n1[i] - '0' : 0;
+	int digit2 = j >= 0 ? n2[j] - '0' : 0;
+	int sum = digit1 + digit2 + carry;
+
+	r[k++] = (sum % 10) + '0';
+
+
+	carry = sum / 10;
+	i--;
+	j--;
 	}
 	r[k] = '\0';
 
-	for (i = 0, j = k - 1; i < j; i++, j--)
+	int lenr = strlen(r);
+
+	for (int i = 0; i < lenr / 2; i++)
 	{
-		char tmp = r[i];
-		r[i] = r[j];
-		r[j] = tmp;
+	char temp = r[i];
+
+	r[i] = r[lenr - 1 - i];
+	r[lenr - 1 - i] = temp;
 	}
 
 	return (r);
