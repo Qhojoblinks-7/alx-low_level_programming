aa3f749ca38793d2595a5cbf9264111c9d2bf234
Qhojojblinks-7 <immanueleshun9@gmail.com>
2023-03-08 07:55:00 -0800

    printing a value using recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - a function that prints a string
+ * followed by a new line using recursion
+ * @s: the string to be printed
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		-putchar('\n');
+		return;
+	}
+	-putchar(*s);
+	_puts_recursion(s + 1);
+}
