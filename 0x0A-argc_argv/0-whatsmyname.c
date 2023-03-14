3b978c5762b43d4f3623aeb83a44a60323970a99
Qhojojblinks-7 <immanueleshun9@gmail.com>
2023-03-14 07:42:56 -0700

    the use of argc and argv[]

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,10 +9,9 @@
  * Return: always 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	(argc > 0);
-
 	printf("%s\n", argv[0]);
+
 	return (0);
 }
