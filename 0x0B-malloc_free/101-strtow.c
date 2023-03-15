b3eb1442a66a0b749cb34a916ad5e8fca92924bf
Qhojojblinks-7 <immanueleshun9@gmail.com>
2023-03-14 17:45:24 -0700

    the use of malloc and free in C

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,60 +1,102 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
+
+int word_len(char *str);
+int count_words(char *str);
+char **strtow(char *str);
 
 /**
- * _strlen - returns the length of a string
- * @s: string
- * Return: length
+ * word_len - Locates the index marking the end of the
+ *            first word contained within a string.
+ * @str: The string to be searched.
+ *
+ * Return: The index marking the end of the initial word pointed to by str.
  */
-
-int _strlen(char *s)
+int word_len(char *str)
 {
-	int len = 0;
+	int index = 0, len = 0;
 
-	while (*s != '\0')
-		len++, s++;
+	while (*(str + index) && *(str + index) != ' ')
+	{
+		len++;
+		index++;
+	}
 
 	return (len);
 }
 
 /**
- * argstostr - concatenates all the arguments of your program
- * @ac: argc
- * @av: arguments
- * Return: pointer to array
+ * count_words - Counts the number of words contained within a string.
+ * @str: The string to be searched.
+ *
+ * Return: The number of words contained within str.
  */
-
-char *argstostr(int ac, char **av)
+int count_words(char *str)
 {
-	char *s;
-	int len = 0, i, j, k = 0;
+	int index = 0, words = 0, len = 0;
 
-	if (ac == 0 || av == NULL)
-		return (NULL);
+	for (index = 0; *(str + index); index++)
+		len++;
 
-	for (i = 0; i < ac; i++)
+	for (index = 0; index < len; index++)
 	{
-		len += _strlen(av[i]);
-	}
-	len += (ac + 1);
-
-	s = malloc(len * sizeof(char));
-
-	if (s == NULL)
-	{
-		free(s);
-		return (NULL);
-	}
-
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; j < _strlen(av[i]); j++)
+		if (*(str + index) != ' ')
 		{
-			s[k++] = av[i][j];
+			words++;
+			index += word_len(str + index);
 		}
-		s[k++] = '\n';
 	}
 
-	return (s);
+	return (words);
+}
+
+/**
+ * strtow - Splits a string into words.
+ * @str: The string to be split.
+ *
+ * Return: If str = NULL, str = "", or the function fails - NULL.
+ *         Otherwise - a pointer to an array of strings (words).
+ */
+char **strtow(char *str)
+{
+	char **strings;
+	int index = 0, words, w, letters, l;
+
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+
+	words = count_words(str);
+	if (words == 0)
+		return (NULL);
+
+	strings = malloc(sizeof(char *) * (words + 1));
+	if (strings == NULL)
+		return (NULL);
+
+	for (w = 0; w < words; w++)
+	{
+		while (str[index] == ' ')
+			index++;
+
+		letters = word_len(str + index);
+
+		strings[w] = malloc(sizeof(char) * (letters + 1));
+
+		if (strings[w] == NULL)
+		{
+			for (; w >= 0; w--)
+				free(strings[w]);
+
+			free(strings);
+			return (NULL);
+		}
+
+		for (l = 0; l < letters; l++)
+			strings[w][l] = str[index++];
+
+		strings[w][l] = '\0';
+	}
+	strings[w] = NULL;
+
+	return (strings);
 }
