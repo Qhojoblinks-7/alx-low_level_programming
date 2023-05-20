078914442abe2e5cf9bb697c5201cc3964461873
Qhojoblinks-7 <immanueleshun9@gmail.com>
2023-05-20 06:11:59 -0700

    some repairs though i couldent find all mistakes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,13 +4,14 @@
  * main - the main functionn that prints a string
  * to stadard error using fprintf() as an
  * output function.
- * the output function used here prints a string to a file,
+ * the output function used here prints
+ * a string to a file,
  * of which stderr and stdout are consised.
  * Return: it returns 1
  */
 
 int main(void)
 {
-	fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n");
 	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,10 +5,11 @@
  * This is a program that prints esactly
  * "with proper grammar, but the outcome is a piece of art,"
  * using the printf() function.
+ * Return: 0
  */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
