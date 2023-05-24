8e4540be927b7ce72c32283965b5e4acff4c2881
Qhojoblinks-7 <immanueleshun9@gmail.com>
2023-05-23 21:58:13 -0700

    some corrections made

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,7 +13,7 @@ void print_alphabet_x10(void)
 	int alpha_lower_x10;
 	int i;
 
-	for (alpha_lower_x10 = 0; alpha_lower_x10 <= 10; alpha_lower_x10++)
+	for (alpha_lower_x10 = 0; alpha_lower_x10 < 10; alpha_lower_x10++)
 	{
 		for (i = 97; i <= 122; i++)
 		{
