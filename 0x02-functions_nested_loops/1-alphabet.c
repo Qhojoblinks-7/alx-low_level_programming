ffb1a0acd9fc0fb4ca889474393194218f37fafa
Qhojoblinks-7 <immanueleshun9@gmail.com>
2023-05-23 21:56:58 -0700

    some corrections made

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -12,7 +12,7 @@ void print_alphabet(void)
 {
 	int alpha_lower;
 
-	for (alpha_lower = '97'; alpha_lower <= '122'; alpha_lower++)
+	for (alpha_lower = 97; alpha_lower <= 122; alpha_lower++)
 	{
 		_putchar((char)alpha_lower);
 	}
