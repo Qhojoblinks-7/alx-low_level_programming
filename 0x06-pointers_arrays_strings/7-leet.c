535520e07a51f8357ad7853df68f6bedea0607fe
Qhojoblinks-7 <immanueleshun9@gmail.com>
2023-06-01 23:10:12 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,8 +14,8 @@ char *leet(char *str)
 {
 	int i = 0;
 	int j = 0;
-	int *leetchar[] = "aAeEoOtTlL";
-	int *leet_digit[] = "4433007711";
+	char leetchar[] = "aAeEoOtTlL";
+	char leet_digit[] = "4433007711";
 
 	while (str[i] != '\0')
 	{
