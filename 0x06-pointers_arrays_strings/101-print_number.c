dff0119011ed420a13fb7396f13cdc5b826177eb
Qhojoblinks-7 <immanueleshun9@gmail.com>
2023-06-04 15:21:27 -0700

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/** print_number - A fuction that prints an integer without using
+ * long ints.
+ *
+ * @n: number to be printed
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	int divisor = 1;
+	int temp = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	while (temp /= 10)
+	{
+		divisor *= 10;
+	}
+
+
+	do
+
+	{
+		_putchar('0' + n / divisor);
+		n %= divisor;
+	       divisor	/= 10;
+
+	} while ( divisor != 0);
+}
