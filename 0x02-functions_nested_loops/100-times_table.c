57f6009ba37c71cce7db53e84fc92bfba0764908
Qhojoblink-7 <immanueleshun9@gmail.com>
2023-07-18 10:10:56 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -37,7 +37,6 @@ void print_times_table(int n)
 
                 if (product < 10)
                 {
-                    _putchar(' ');
                 }
                 else if (product < 100)
                 {
