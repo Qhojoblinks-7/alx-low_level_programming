8050acccfe9f12412e0652c7fad403cc0f420ebe
Qhojoblink-7 <immanueleshun9@gmail.com>
2023-07-18 10:07:13 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,7 +17,7 @@ void print_times_table(int n)
 
     if (n < 0 || n > 15)
     {
-        return; // Stop execution for invalid input.
+        return; /* Stop execution for invalid input.*/
     }
 
     for (num1 = 0; num1 <= n; num1++)
