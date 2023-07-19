aa0f9c05855b4fa3db54a27d0357d36b858f3f6d
Qhojoblink-7 <immanueleshun9@gmail.com>
2023-07-18 19:18:23 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,7 +11,7 @@ int main(void)
 
 	printf("%lu, %lu, ", first_num, second_num);
 
-	for (count = 2; count < 98; count++)
+	for (count = 2; count < 99; count++)
 	{
 		next = first_num + second_num;
 
