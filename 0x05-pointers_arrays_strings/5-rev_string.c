c8c6c08be4d3bb9a1729474b01d8135cd6aa6b0f
Qhojoblink-7 <immanueleshun9@gmail.com>
2023-07-24 13:43:04 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,35 +10,17 @@
 
 void rev_string(char *s)
 {
-	char temp; /* to store string temprarily */
-	int length; /* to help check the length of the string */
-	int start = 0; /* to mark the begining of the string */
-	int end = 0; /* to mark the end of the string */
-	int i;
-	/* calculating the length of the string */
-	while (s[length] != '\0')
+	int i = 0;
+
+	while (s[i] != '\0')
 	{
-		length++;
+		i++;
 	}
-	end = length -1;
 
-	/* now we do the swapping */
-	while (start < end)
+	while (i > 0)
 	{
-		temp = s[start]; /* the string at the begining is stored in tmp */
-		s[start] = s[end]; /* the string at the end is swapped to begin */
-		s[end] = temp; /* the begining of the string is now stored at the end */
-		start++; /* move to the next character */
-		end--; /* move to the next character backwards */
-
-
-		/* printing the reversed string */
-		i = 0;
-		while (s[i] != '\0')
-		{
-			_putchar(s[i]);
-			i++;
-		}
-		_putchar('\n');
+		--i;
+		_putchar(s[i]);
 	}
+	_putchar('\n);
 }
