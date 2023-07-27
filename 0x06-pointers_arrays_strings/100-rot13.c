cef3be50557489311599900bf2c9b09189b50191
Qhojoblink-7 <immanueleshun9@gmail.com>
2023-07-27 01:06:12 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 
 /**
@@ -12,16 +13,39 @@
 char *rot13(char *str)
 {
 	int i;
+	char *encoded_str;
+
+	if (str == NULL)
+		return NULL;
+
+	int len = 0;
+	while (str[len] != '\0)
+	{
+		len++;
+	}
+	encoded_str = (char *)malloc(sizeof(char) * (len + 1));
+	if (encoded_str == NULL)
+	{
+		return (NULL);
+	}
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		str[i] = (str[i] >= 'A' && str[i] <= 'Z')
-			? ((str[i] - 'A' + 13) % 26 + 'A')
-			: (str[i] >= 'a' && str[i] <= 'z')
-			? ((str[i] - 'a' + 13) % 26 + 'a')
-			: str[i];
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			encoded_str[i] = ((str[i] - 'A' + 13) % 26) + 'A';
+		}
+		else if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			encoded_str[i] = ((str[i] - 'a' + 13) % 26) + 'a';
+		}
+		else
+		{
+			encoded_str[i] = str[i];
+		}
 	}
+	encoded_str[i] = '\0';
 
-	return (str);
+	return (encoded_str);
 }
 		
