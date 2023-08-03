08ca0732d7dc3b4b8db636f88b5ec39246901c0f
Qhojoblink-7 <immanueleshun9@gmail.com>
2023-08-03 16:46:15 -0700

    0x09-static_libraries

---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stddef.h>
+
+char *_strstr(char *haystack, char *needle) {
+    char *h, *n;
+    while (*haystack) {
+        h = haystack;
+        n = needle;
+        while (*n && (*h == *n)) {
+            h++;
+            n++;
+        }
+        if (*n == '\0') {
+            return haystack;
+        }
+        haystack++;
+    }
+    return NULL;
+}
+
---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+char *_strcpy(char *dest, char *src) {
+    char *start = dest;
+    while (*src) {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+    *dest = '\0';
+    return start;
+}
+
