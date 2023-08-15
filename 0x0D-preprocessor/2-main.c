36e2cf49d5f5a4d77663e65fc9af34c7f8674080
Qhojoblinks-7 <immanueleshun9@gmail.com>
2023-08-15 01:56:43 +0000

    0x0D-preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - a function that prints the name of the
+ * function it was compiled from
+ * Return: an int
+ */
+
+int main(void)
+{
+	printf("%s\n, __FILE__");
+
+	return(0);
