f2a11ae3b044d0f68a2e9643f3526a5fa442f7f3
Qhojoblinks-7 <immanueleshun9@gmail.com>
2023-09-04 16:42:25 -0700

    /0x15-file_io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,6 +13,7 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	FILE *file;
 	char *fileBuffer;
 	ssize_t bytes_read = 0, bytes_written;
 
@@ -21,7 +22,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	}
 
-	FILE *file = fopen(filename, "r");
+	file = fopen(filename, "r");
 	{
 		if (file == NULL)
 		{
