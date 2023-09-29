de2861adabcccd626eb2d3f7185d96f51ff93cce
Qhojoblinks-7 <immanueleshun9@gmail.com>
2023-09-28 20:07:43 -0700

    corrections

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,23 +7,21 @@
 void print_binary(unsigned long int n)
 {
 	int i, count = 0;
-	unsigned long int current;
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
 
 	for (i = 63; i >= 0; i--)
 	{
-		current = n >> i;
-
-		if (current & 1)
+		if ((n >> i) & 1)
 		{
 			_putchar('1');
-			count++;
-		}
-		else if (count)
-		{
-			_putchar('0');
+			count = 1;
 		}
 
-		if (!count)
+		else if (count)
 		{
 			_putchar('0');
 		}
